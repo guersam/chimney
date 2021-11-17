82da3980b090a1f0b33c815119f5624e4bb4896f
Jisoo Park <jisoo.park@doomoolmori.com>
2021-11-17 22:51:31 +0900

    wip - crossbuild

--- chimney/src/main/scala/io/scalaland/chimney/dsl/TransformerInto.scala
+++ chimney/src/main/scala/io/scalaland/chimney/dsl/TransformerInto.scala
@@ -2,9 +2,6 @@ package io.scalaland.chimney.dsl
 
 import io.scalaland.chimney.internal.TransformerCfg._
 import io.scalaland.chimney.internal._
-import io.scalaland.chimney.internal.macros.dsl.{TransformerBlackboxMacros, TransformerIntoWhiteboxMacros}
-
-import scala.language.experimental.macros
 
 /** Provides DSL for configuring [[io.scalaland.chimney.Transformer]]'s
   * generation and using the result to transform value at the same time
@@ -18,7 +15,8 @@ import scala.language.experimental.macros
 final class TransformerInto[From, To, C <: TransformerCfg, Flags <: TransformerFlags](
     val source: From,
     val td: TransformerDefinition[From, To, C, Flags]
-) extends FlagsDsl[Lambda[`F1 <: TransformerFlags` => TransformerInto[From, To, C, F1]], Flags] {
+) extends FlagsDsl[Lambda[`F1 <: TransformerFlags` => TransformerInto[From, To, C, F1]], Flags]
+     with TransformerIntoPlatformSpecific[From, To, C, Flags] {
 
   /** Lifts current transformation with provided type constructor `F`.
     *
@@ -31,116 +29,6 @@ final class TransformerInto[From, To, C <: TransformerCfg, Flags <: TransformerF
   def lift[F[+_]]: TransformerFInto[F, From, To, WrapperType[F, C], Flags] =
     new TransformerFInto[F, From, To, WrapperType[F, C], Flags](source, td.lift[F])
 
-  /** Use `value` provided here for field picked using `selector`.
-    *
-    * By default if `From` is missing field picked by `selector` compilation fails.
-    *
-    * @see [[https://scalalandio.github.io/chimney/transformers/customizing-transformers.html#providing-missing-values]] for more details
-    * @return [[io.scalaland.chimney.dsl.TransformerInto]]
-    */
-  def withFieldConst[T, U](selector: To => T, value: U): TransformerInto[From, To, _ <: TransformerCfg, Flags] =
-    macro TransformerIntoWhiteboxMacros.withFieldConstImpl
-
-  /** Use wrapped `value` provided here for field picked using `selector`.
-    *
-    * By default if `From` is missing field picked by `selector` compilation fails.
-    *
-    * @see [[https://scalalandio.github.io/chimney/transformers/customizing-transformers.html#providing-missing-values]] for more details
-    * @param selector target field in `To`, defined like `_.name`
-    * @param value    constant value to use for the target field
-    * @return [[io.scalaland.chimney.dsl.TransformerFInto]]
-    */
-  def withFieldConstF[F[+_], T, U](
-      selector: To => T,
-      value: F[U]
-  ): TransformerFInto[F, From, To, _ <: TransformerCfg, Flags] =
-    macro TransformerIntoWhiteboxMacros.withFieldConstFImpl[F]
-
-  /** Use `map` provided here to compute value of field picked using `selector`.
-    *
-    * By default if `From` is missing field picked by `selector` compilation fails.
-    *
-    * @see [[https://scalalandio.github.io/chimney/transformers/customizing-transformers.html#providing-missing-values]] for more details
-    * @param selector target field in `To`, defined like `_.name`
-    * @param map      function used to compute value of the target field
-    * @return [[io.scalaland.chimney.dsl.TransformerInto]]
-    * */
-  def withFieldComputed[T, U](
-      selector: To => T,
-      map: From => U
-  ): TransformerInto[From, To, _ <: TransformerCfg, Flags] =
-    macro TransformerIntoWhiteboxMacros.withFieldComputedImpl
-
-  /** Use `map` provided here to compute wrapped value of field picked using `selector`.
-    *
-    * By default if `From` is missing field picked by `selector` compilation fails.
-    *
-    * @see [[https://scalalandio.github.io/chimney/transformers/customizing-transformers.html#providing-missing-values]] for more details
-    * @param selector target field in `To`, defined like `_.name`
-    * @param map      function used to compute value of the target field
-    * @return [[io.scalaland.chimney.dsl.TransformerFInto]]
-    */
-  def withFieldComputedF[F[+_], T, U](
-      selector: To => T,
-      map: From => F[U]
-  ): TransformerFInto[F, From, To, _ <: TransformerCfg, Flags] =
-    macro TransformerIntoWhiteboxMacros.withFieldComputedFImpl[F]
-
-  /** Use `selectorFrom` field in `From` to obtain the value of `selectorTo` field in `To`
-    *
-    * By default if `From` is missing field picked by `selectorTo` compilation fails.
-    *
-    * @see [[https://scalalandio.github.io/chimney/transformers/customizing-transformers.html#fields-renaming]] for more details
-    * @param selectorFrom source field in `From`, defined like `_.originalName`
-    * @param selectorTo   target field in `To`, defined like `_.newName`
-    * @return [[io.scalaland.chimney.dsl.TransformerInto]]
-    * */
-  def withFieldRenamed[T, U](
-      selectorFrom: From => T,
-      selectorTo: To => U
-  ): TransformerInto[From, To, _ <: TransformerCfg, Flags] =
-    macro TransformerIntoWhiteboxMacros.withFieldRenamedImpl
-
-  /** Use `f` to calculate the (missing) coproduct instance when mapping one coproduct into another
-    *
-    * By default if mapping one coproduct in `From` into another coproduct in `To` derivation
-    * expects that coproducts will have matching names of its components, and for every component
-    * in `To` field's type there is matching component in `From` type. If some component is missing
-    * it will fail.
-    *
-    * @see [[https://scalalandio.github.io/chimney/transformers/customizing-transformers.html#transforming-coproducts]] for more details
-    * @param f function to calculate values of components that cannot be mapped automatically
-    * @return [[io.scalaland.chimney.dsl.TransformerInto]]
-    */
-  def withCoproductInstance[Inst](f: Inst => To): TransformerInto[From, To, _ <: TransformerCfg, Flags] =
-    macro TransformerIntoWhiteboxMacros.withCoproductInstanceImpl
-
-  /** Use `f` to calculate the (missing) wrapped coproduct instance when mapping one coproduct into another
-    *
-    * By default if mapping one coproduct in `From` into another coproduct in `To` derivation
-    * expects that coproducts to have matching names of its components, and for every component
-    * in `To` field's type there is matching component in `From` type. If some component is missing
-    * it fails compilation unless provided replacement with this operation.
-    *
-    * @see [[https://scalalandio.github.io/chimney/transformers/customizing-transformers.html#transforming-coproducts]] for more details
-    * @param f function to calculate values of components that cannot be mapped automatically
-    * @return [[io.scalaland.chimney.dsl.TransformerFInto]]
-    */
-  def withCoproductInstanceF[F[+_], Inst](f: Inst => F[To]): TransformerFInto[F, From, To, _ <: TransformerCfg, Flags] =
-    macro TransformerIntoWhiteboxMacros.withCoproductInstanceFImpl[F]
-
-  /** Apply configured transformation in-place.
-    *
-    * It runs macro that tries to derive instance of `Transformer[From, To]`
-    * and immediately apply it to captured `source` value.
-    * When transformation can't be derived, it results with compilation error.
-    *
-    * @return transformed value of type `To`
-    */
-  def transform[ScopeFlags <: TransformerFlags](
-      implicit tc: io.scalaland.chimney.dsl.TransformerConfiguration[ScopeFlags]
-  ): To =
-    macro TransformerBlackboxMacros.transformImpl[From, To, C, Flags, ScopeFlags]
 
   /** Used internally by macro. Please don't use in your code.
     */
